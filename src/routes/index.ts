fe5e3350f4b3a5e3b1d838f79b8041d0d7283a81
Amri Mufti <amrimuvti@gmail.com>
2023-09-22 19:13:32 +0700

    feat: add main entry:bricks:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import authRoutes from './auth';
 import deviceRoutes from './devices';
 import userRoutes from './users';
@@ -6,6 +6,11 @@ import sessionRoutes from './sessions';
 import authMiddleware from '../middleware/auth';
 
 const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('Forwardin Jaya Jaya Jaya!');
+});
+
 router.use('/auth', authRoutes);
 router.use('/sessions', authMiddleware, sessionRoutes);
 router.use('/devices', authMiddleware, deviceRoutes);
