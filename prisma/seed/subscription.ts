a32e60b1118e35660b2e68b009dac2bc65b5a8e9
Amri Mufti <amrimuvti@gmail.com>
2023-10-10 13:57:25 +0700

    db(subscription): update subscription type seeder

--- prisma/seed/subscription.ts
+++ prisma/seed/subscription.ts
@@ -4,13 +4,27 @@ async function seedSubscriptions(prisma: PrismaClient) {
     await prisma.subscription.createMany({
         data: [
             {
-                name: 'Free',
-                price: 0,
+                name: 'Starter',
+                monthlyPrice: 0,
+                yearlyPrice: 0,
+                isAvailable: true,
+            },
+            {
+                name: 'Basic',
+                monthlyPrice: 65000,
+                yearlyPrice: 650000,
                 isAvailable: true,
             },
             {
                 name: 'Premium',
-                price: 19.99,
+                monthlyPrice: 76000,
+                yearlyPrice: 800000,
+                isAvailable: true,
+            },
+            {
+                name: 'Pro',
+                monthlyPrice: 86000,
+                yearlyPrice: 900000,
                 isAvailable: true,
             },
         ],
