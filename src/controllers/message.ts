cd2d2b35b5a092ef7e65fdec4fe0394e8c0553d6
Amri Mufti <amrimuvti@gmail.com>
2023-11-02 00:20:47 +0700

    fix(messenger): sort messages in descending order

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -335,7 +335,7 @@ export const getMessengerList: RequestHandler = async (req, res) => {
         logger.debug(allMessages);
 
         // Sort the combined messages by timestamp (receivedAt or createdAt)
-        allMessages.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
+        allMessages.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 
         // Create a map to track unique recipients and their most recent timestamps
         const uniqueRecipients = new Map();
