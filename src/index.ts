942c712d1f66d0ece9112310645a68a51f946d19
Amri Mufti <amrimuvti@gmail.com>
2023-11-02 11:30:07 +0700

    feat(socket): establish connection with client

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import pinoHttp from 'pino-http';
 import prisma from './utils/db';
 import { init } from './instance';
 import bodyParser from 'body-parser';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 app.use(pinoHttp({ logger }));
@@ -18,6 +20,29 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
 
 app.use('/', routes);
 app.all('*', (req, res) => res.status(404).json({ error: 'URL not found' }));
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+    logger.warn(socket.id);
+
+    const data = {
+        deviceId: '8631e4e7-b399-4b71-b741-a865a60df877',
+        status: 'connecting',
+    };
+    socket.emit('statusUpdate', data);
+
+    socket.on('message', (message) => {
+        logger.warn(`WebSocket client sent a message: ${message}`);
+        socket.send(`WebSocket server received: ${message}`);
+    });
+
+    socket.on('close', () => {
+        console.log('WebSocket client disconnected');
+    });
+});
 
 const host = process.env.HOST || '0.0.0.0';
 const port = Number(process.env.PORT || 3000);
@@ -35,7 +60,7 @@ prisma
 
 (async () => {
     await init();
-    app.listen(port, host, listener);
+    server.listen(port, host, listener);
 })();
 
 export default app;
