c88974f612e9bbd2b439ea6cf2d5739b3d1988f0
Amri Mufti <amrimuvti@gmail.com>
2023-11-25 07:19:19 +0700

    feat: implement create & display all templates

---
+++ src/controllers/template.ts
@@ -0,0 +1,46 @@
+import { RequestHandler } from 'express';
+import prisma from '../utils/db';
+import logger from '../config/logger';
+
+export const createTemplate: RequestHandler = async (req, res) => {
+    try {
+        const { name, message, userId } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const template = await prisma.template.create({
+            data: {
+                name,
+                message,
+                userId: user.pkId,
+            },
+        });
+        res.status(201).json({ message: 'Template created successfully', template });
+    } catch (error) {
+        logger.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const getTemplates: RequestHandler = async (req, res) => {
+    const userId = req.authenticatedUser.pkId;
+    const privilegeId = req.privilege.pkId;
+
+    try {
+        const templates = await prisma.template.findMany({
+            where: {
+                userId: privilegeId !== Number(process.env.SUPER_ADMIN_ID) ? userId : undefined,
+            },
+        });
+        res.status(200).json(templates);
+    } catch (error) {
+        logger.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
---
+++ src/routes/templates.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import * as controller from '../controllers/template';
+import { checkPrivilege } from '../middleware/auth';
+
+const router = express.Router();
+
+router.use(checkPrivilege('template'));
+router.post('/', controller.createTemplate);
+router.get('/', controller.getTemplates);
+
+export default router;
