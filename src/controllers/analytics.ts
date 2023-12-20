d2b5c42e9c55d6a8512cd77fba3d8dcd8594849a
Amri Mufti <amrimuvti@gmail.com>
2023-12-20 15:05:14 +0700

    feat: add message analytics

--- src/controllers/analytics.ts
+++ src/controllers/analytics.ts
@@ -39,3 +39,77 @@ export const getOrder: RequestHandler = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+export const getMessageStatistics: RequestHandler = async (req, res) => {
+    try {
+        const deviceId = req.query.deviceId as string;
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+
+        const sessions = await prisma.session.findFirst({
+            where: { device: { id: deviceId } },
+        });
+
+        if (!sessions) {
+            return res.status(404).json({ message: 'Session for this device not found' });
+        }
+
+        const todayIncomingMessageCount = await prisma.incomingMessage.count({
+            where: {
+                sessionId: sessions.sessionId,
+                createdAt: {
+                    gte: today,
+                    lt: tomorrow,
+                },
+            },
+        });
+
+        const todayFailedMessageCount = await prisma.outgoingMessage.count({
+            where: {
+                sessionId: sessions.sessionId,
+                status: '0',
+                createdAt: {
+                    gte: today,
+                    lt: tomorrow,
+                },
+            },
+        });
+
+        const todayOutgoingMessageCount = await prisma.outgoingMessage.count({
+            where: {
+                sessionId: sessions.sessionId,
+                createdAt: {
+                    gte: today,
+                    lt: tomorrow,
+                },
+            },
+        });
+
+        const incomingMessageCount = await prisma.incomingMessage.count({
+            where: { sessionId: sessions.sessionId },
+        });
+
+        const failedMessageCount = await prisma.outgoingMessage.count({
+            where: { sessionId: sessions.sessionId, status: '0' },
+        });
+
+        const outgoingMessageCount = await prisma.outgoingMessage.count({
+            where: { sessionId: sessions.sessionId },
+        });
+
+        res.status(200).json({
+            todayIncomingMessageCount,
+            todayOutgoingMessageCount,
+            todayFailedMessageCount,
+            incomingMessageCount,
+            outgoingMessageCount,
+            failedMessageCount,
+        });
+    } catch (error) {
+        logger.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.use(checkPrivilege('analytics'));
 router.get('/orders/:customerServiceId', controller.getOrder);
+router.get('/messages', controller.getMessageStatistics);
 
 export default router;
