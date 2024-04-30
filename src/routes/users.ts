1a903e2d9891ac070c7afd19093ae81caf6aeb5c
“nameisjar” <nthia4@gmail.com>
2024-04-30 08:24:27 +0700

    fix to position endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,12 +7,12 @@ const router = Router();
 router.use(checkPrivilege('user'));
 router.get('/:userId', controller.getUserProfile);
 router.get('/:userId/subscription', controller.getUserSubscriptionDetail);
+router.get('/:userId/notifications', controller.getNotifications);
+router.get('/:userId/notifications/:notificationId', controller.getNotificationById);
 router.patch('/:userId', controller.updateUser);
 router.patch('/change-email/:userId', controller.changeEmail);
 router.patch('/change-phone-number/:userId', controller.changePhoneNumber);
 router.get('/customer-services/:userId', controller.getCustomerServices);
-router.get('/:userId/notifications', controller.getNotifications);
-router.get('/:userId/notifications/:notificationId', controller.getNotificationById);
 
 router.use(isEmailVerified);
 router.delete('/:userId/delete', controller.deleteUser);
