381f6867b0b12d3c9e75adfe3034446d7f589f80
“nameisjar” <nthia4@gmail.com>
2025-12-15 23:28:27 +0700

    fix chrome version

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -364,7 +364,7 @@ export async function createInstance(options: createInstanceOptions) {
     // back here: adjust SocketConfig such as turn off always online
     const sock = makeWASocket({
         // printQRInTerminal removed due to deprecation; handled manually in connection.update
-        browser: ['Autosender', 'Chrome', '10.0'],
+        browser: ['Autosender', 'Chrome', '143.0.0.0'],
         ...socketConfig,
         auth: {
             creds: state.creds,
