45e5c3a349554e96271daf149149c2b26b5bf78b
“nameisjar” <nthia4@gmail.com>
2025-12-26 13:54:45 +0700

    delete comment

--- scripts/create-seeder.js
+++ scripts/create-seeder.js
@@ -8,7 +8,7 @@ const ws = wb.Sheets[wb.SheetNames[0]];
 const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
 
 // console.log('Total rows:', data.length);
-// console.log('Header:', data[0]);
+
 
 // Generate seeder data
 const seederData = [];
